422ce8dd08e0c281022adb992c80411d2d2957dc
Chuy <jesus.cruz@oracle.com>
2021-06-28 13:05:14 -0500

    Prepare APIURLResources

--- TopQuestionsUIKit/TopQuestionsUIKit/Network/NetworkRequest.swift
+++ TopQuestionsUIKit/TopQuestionsUIKit/Network/NetworkRequest.swift
@@ -11,18 +11,10 @@ import UIKit
 // MARK: - NetworkRequest
 
 
-
-// MARK: - APIResource
-protocol APIResource {
-    associatedtype ModelType
-    var methodPath: String { get }
-    var filter: String? { get }
-}
-
-extension APIResource {
-    var url: URL {
+// MARK: - APIURLResources
+struct APIURLResources {
+    static func getQuestionsURL() -> URL {
         var components = URLComponents(string: "https://api.stackexchange.com/2.2")!
-        components.path = methodPath
         components.queryItems = [
             URLQueryItem(name: "site", value: "stackoverflow"),
             URLQueryItem(name: "order", value: "desc"),
@@ -30,26 +22,19 @@ extension APIResource {
             URLQueryItem(name: "tagged", value: "swiftui"),
             URLQueryItem(name: "pagesize", value: "10")
         ]
-        if let filter = filter {
-            components.queryItems?.append(URLQueryItem(name: "filter", value: filter))
-        }
         return components.url!
     }
-}
-
-// MARK: - QuestionsResource
-struct QuestionsResource: APIResource {
-    typealias ModelType = Question
-    var id: Int?
     
-    var methodPath: String {
-        guard let id = id else {
-            return "/questions"
-        }
-        return "/questions/\(id)"
-    }
-    
-    var filter: String? {
-        id != nil ? "!9_bDDxJY5" : nil
+    static func getQuestionsDetailURL(id: Int) -> URL {
+        var components = URLComponents(string: "https://api.stackexchange.com/2.2/questions/\(id)")!
+        components.queryItems = [
+            URLQueryItem(name: "site", value: "stackoverflow"),
+            URLQueryItem(name: "order", value: "desc"),
+            URLQueryItem(name: "sort", value: "votes"),
+            URLQueryItem(name: "tagged", value: "swiftui"),
+            URLQueryItem(name: "pagesize", value: "10"),
+            URLQueryItem(name: "filter", value: "!9_bDDxJY5")
+        ]
+        return components.url!
     }
 }
