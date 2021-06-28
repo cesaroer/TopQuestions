49ae2bc807fa1b150b0a0069f8944f321cba27d7
Chuy <jesus.cruz@oracle.com>
2021-06-28 13:07:38 -0500

    Clean files

--- TopQuestionsUIKit/TopQuestionsUIKit/DataModel/QuestionDataModel.swift
+++
@@ -1,7 +0,0 @@
-//
-//  QuestionDataModel.swift
-//  TopQuestionsUIKit
-//
-//  Created by Jesus Cruz on 25/06/21.
-//
-
--- TopQuestionsUIKit/TopQuestionsUIKit/DataModel/QuestionsDataModel.swift
+++
@@ -1,6 +0,0 @@
-//
-//  QuestionsDataModel.swift
-//  TopQuestionsUIKit
-//
-//  Created by Jesus Cruz on 25/06/21.
-//
